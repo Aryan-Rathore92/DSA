06f8195550eae5027158c6e2ff136494fe849a20
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-05-27 14:53:44 +0530

    datatypes and variables

---
+++ Basics/first.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+ int main() {
+    float a = 3.1234567890;
+    double b = 2.1234567890;
+    cout<< setprecision(12)<< "Value of a is = "<< a << "\n" ;
+    cout<< setprecision(12)<< "Value of b is ="<< b;
+
+    return 0;
+ }
