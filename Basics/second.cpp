53ab1f79394eb2ba3439958a81cb9f692c0de601
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-06-17 22:24:57 +0530

    qusetion based on codition

--- Basics/second.cpp
+++ Basics/second.cpp
@@ -1,19 +1,7 @@
 #include<iostream>
 using namespace std;
-  
-int main() {
-    int age;
-    int result;
-    cout<< "Enter the age : ";
-    cin>>age;
 
-    result = (age >= 18) ? 1 : 0; 
-
-    if(result == 1){
-        cout<< "you are adult";
-    }
-    else{
-        cout<< "You are not adult !";
-    }
+int main(){
+    
     return 0;
 }
