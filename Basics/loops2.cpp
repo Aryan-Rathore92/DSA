1a82aba53491a41a29d26c5cbf7238be8fd2bae6
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-06-19 21:33:15 +0530

    reverse a number

--- Basics/loops2.cpp
+++ Basics/loops2.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-    int num, res=0;
+    int num, result=0;
    cout<< "Enter a number have digit :";
    cin>>num;
    while(num >=1){
     int rem = num%10;
-        res = res*10 + rem;
+        result = result*10 + rem;
         num = num/10;
    }
-   cout << res;
+   cout << result;
    return 0;
 }
