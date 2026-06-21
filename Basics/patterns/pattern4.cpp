6c9f10bfa0126924656b90cd1688f4a13441eb76
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-06-21 08:59:36 +0530

    pattern4 completed

--- Basics/patterns/pattern4.cpp
+++ Basics/patterns/pattern4.cpp
@@ -4,12 +4,7 @@ using namespace std;
 int main() {
     for(int i=1; i<=4; i++){
         for(int j=1; i>=j; j++){
-            if(j==1){
-                cout<< i;
-            }
-            if(j > 1){
-                cout<< j;
-            }
+            cout<< j;
         }
         cout << endl;
     }
