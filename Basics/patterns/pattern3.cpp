0a916120cf2c9d2e501c58f402e6e294512a3821
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-06-22 09:21:27 +0530

    pattern3 changed

--- Basics/patterns/pattern3.cpp
+++ Basics/patterns/pattern3.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 
 int main() {
-    for(int i=1; i<=4; i++){
-        for(int j=i; j<=4; j++){
+    int n=4;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=(n-i + 1); j++){
             cout<< "*";
         }
         cout<< endl;
