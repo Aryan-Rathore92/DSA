157995d4ca823d94cbfbb144530d7ba513d2f1ad
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-06-24 08:46:13 +0530

    pattern9 completed

--- Basics/patterns/pattern9.cpp
+++ Basics/patterns/pattern9.cpp
@@ -3,21 +3,23 @@ using namespace std;
 
 int main() {
     int n=4;
+    // pyramid-01
     for(int i=1; i<=n; i++){
         for(int j=1; (n-i)>=j; j++){
-            cout << " ";
+            cout << " "; // first-spaces
         }
         for(int j=1; (2*i-1)>=j; j++){
-            cout << "*";
+            cout << "*"; // first-stars
         }
         cout << endl;
     }
+    // pyramid-02
     for(int i=n; i>=1; i--){
         for(int j=1; (n-i)>=j; j++){
-            cout << " ";
+            cout << " "; // second-spaces
         }
         for(int j=1; (2*i-1)>=j; j++){
-           cout << "*";
+           cout << "*"; // second-stars
         }
         cout << endl;
     }
