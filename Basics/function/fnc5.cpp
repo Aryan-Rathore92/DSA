7139cd8b5aa66f8e737b07381d29ca3474cb6fd0
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-06-27 09:28:59 +0530

    Calculate binomial cofficent

--- Basics/function/fnc5.cpp
+++ Basics/function/fnc5.cpp
@@ -1,24 +1,24 @@
 #include<iostream>
 using namespace std;
 
-int factorial(int n){ // calculate factorial
-   int fact =1;
-   for(int i=1; i<=n; i++){
-    fact *= i;
-   }
-   return fact;
+int factorial(int n){
+    int fact = 1;
+    for(int i=1; i<=n; i++){
+        fact *= i;
+    }
+        return fact;
 }
 
-int binCoff(int n, int r){ // calculate binomial cofficent
+int binomialCofficent(int n, int r){
     int val1 = factorial(n);
     int val2 = factorial(r);
     int val3 = factorial(n-r);
-    int result = val1 / (val2*val3);
+    int result = val1/(val2*val3);
     return result;
 }
 
 int main(){
-    int res = binCoff(4,2);
-    cout << res;
+    int res =  binomialCofficent(4,2);
+    cout << res << endl;
     return 0;
 }
