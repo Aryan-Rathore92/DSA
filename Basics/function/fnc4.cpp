5a396e407ef4587744a2cfa1c31e5df6eda70391
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-06-27 09:11:42 +0530

    method 2 of check prime

--- Basics/function/fnc4.cpp
+++ Basics/function/fnc4.cpp
@@ -1,20 +1,37 @@
 #include<iostream>
 using namespace std;
 
-char isPrime(int num){
-    if(num%2 == 0){
-        cout << "Number " << num << " is an co-prime no.";
-    } else if(num == 1){
-       cout << "Number " << num << " is an co-prime no.";
+// method-01
+// char isPrime(int num){
+//     if(num%2 == 0){
+//         cout << "Number " << num << " is an co-prime no.";
+//     } else if(num == 1){
+//        cout << "Number " << num << " is an co-prime no.";
+//     }
+//     else{
+//         cout << "Number " << num << " is an prime no.";
+//     }
+// }
+
+// method-02
+
+bool checkPrime(int n){
+    if(n==1){
+        return false;
     }
-    else{
-        cout << "Number " << num << " is an prime no.";
+
+    for(int i=2; i<=(n-1); i++){
+        if(n%i==0){ // co-prime
+            return false;
+        }
     }
+    return true;
 }
+
 int main(){
     int a;
     cout << "Enter the number : ";
     cin >> a;
-    isPrime(a);
+    cout << checkPrime(a);
     return 0;
 }
