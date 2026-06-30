063606d1170f43af76f348ddd6c2f9ab5689ee9b
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-06-30 23:28:57 +0530

    update

--- Basics/binary_number/binary1.cpp
+++ Basics/binary_number/binary1.cpp
@@ -2,12 +2,17 @@
 #include<math.h>
 using namespace std;
 
-int square(int n){
-    return pow(2, n);
-}
+// int square(int n){
+//     return pow(2, n); // check range of int
+// }
+
+// int main() {
+//     int res = square(31);
+//     cout << res << endl;
+//     return 0;
+// }
 
 int main() {
-    int res = square(31);
-    cout << res << endl;
+    
     return 0;
 }
