e75cc1a20d5a589adea8241fff813425615b8f91
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-07-01 22:51:14 +0530

    pointer

--- pointer/pointer2.cpp
+++ pointer/pointer2.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main(){
-    
+    int  a = 10;
+    int *ptr = &a;
+    int **ptr2 = &ptr; 
+    int ***ptr3 = &ptr2;
+    cout << ptr2 << " = " << ptr << endl;
+    cout << ptr3 << " = " << ptr2 << endl;
     return 0;
 }
