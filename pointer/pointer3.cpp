f8b964625e0f3e97a6b4a456aac604a9cd7e3850
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-07-01 23:53:40 +0530

    update

--- pointer/pointer3.cpp
+++ pointer/pointer3.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 using namespace std;
 
-int main() {
-    int a = 10;
+int main(){
+    int  a = 10;
     int *ptr = &a;
-    int result = *(ptr); // derefrence operator
+    int result = *(ptr);
     cout << result << endl;
 
-    *(ptr) = 20; // new value asign through derefrence operator
+    *(ptr) = 20;
     cout << a << endl;
+    return 0;
 }
