b22a9cc9340e6628222828bf0a9500f80fdaac63
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-07-02 23:32:25 +0530

    update

--- pointer/pointer5.cpp
+++ pointer/pointer5.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 //  Pass by Value
-// void changeA(int a){
-//    a = 20; // here making an copy of a.
-//    cout << a << endl;
-// }
+void changeA(int a){
+   a = 20; // here making an copy of a.
+   cout << a << endl;
+}
 
 // Pass by refrence
  void changeA(int *ptr){
    *(ptr) = 20; // here changing original a.
  }
- 
+
 int main(){
     int a =10;
     changeA(&a);
