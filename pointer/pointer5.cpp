477c48df947a41ae92c7cc5a0cd023f2dadce5b1
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-07-02 23:20:34 +0530

    call by value and refrence

--- pointer/pointer5.cpp
+++ pointer/pointer5.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
 using namespace std;
 
-void changeA(int a){
-   a = 20;
-   cout << a << endl;
-}
+//  Pass by Value
+// void changeA(int a){
+//    a = 20; // here making an copy of a.
+//    cout << a << endl;
+// }
 
+// Pass by refrence
+ void changeA(int *ptr){
+   *(ptr) = 20; // here changing original a.
+ }
+ 
 int main(){
     int a =10;
-    changeA(a);
+    changeA(&a);
     cout << a << endl;
     return 0;
 }
