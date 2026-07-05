8bd92f48b4c2f90d527218ec44413fdab7c52c31
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-07-05 21:50:36 +0530

    array as refrence

--- array/arr4.cpp
+++ array/arr4.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
 using namespace std;
 
+void func(int arr[]){ // passing array as refrence
+    arr[2] = 1000;
+}
+
+void func2(int *ptr){ // This function is also same like func, Here passing address to the pointer
+    ptr[0] = 2000;
+}
+
 int main() {
-    int arr[5] = {1,2,3,4,5};
-    cout<< *arr+4;
+    int arr[] = {1,2,3,4,5};
+     func2(arr); // passing array name is equivalent to passing the pointer
+     cout << arr << endl;
+     cout << arr[0] << endl;
+     cout << arr[2] << endl;
     return 0;
 }
