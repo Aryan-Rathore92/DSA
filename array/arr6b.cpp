388a14febdff0e106844e7cf040d601716f03555
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-07-06 23:26:50 +0530

    2 pointer method for reverse array

--- array/arr6b.cpp
+++ array/arr6b.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
 using namespace std;
 
+int printArr(int *ptr, int n){
+  for(int i=0; i<n; i++){
+    cout << ptr[i] << ",";
+  }
+  cout << endl;
+}
+
 
 int main() {
-    
+    int arr[]= {5,4,3,9,2};
+    int n = sizeof(arr)/sizeof(int);
+    int start = 0, end = n-1;
+
+    while(start < end){
+        int temp = arr[end];
+        arr[end] = arr[start];
+        arr[start] = temp;
+        start++;
+        end--;
+    }
+
+    printArr(arr,n);
     return 0;
 }
