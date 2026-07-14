c2d558f188d504e9491a9a25031e9151fc890387
Aryan-Rathore92 <rathorearyan533@gmail.com>
2026-07-14 23:22:48 +0530

    pointer
    comparisons

--- array/arr13.cpp
+++ array/arr13.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 
 int main() {
-    
+    int arr[]= {1,2,3,4,5};
+    int *ptr1 = arr;
+    int *ptr2 = ptr1 + 3;
+    // cout << (ptr2 > ptr1) << endl; // 1
+    // cout << (ptr2 < ptr1) << endl; // 0
+    // cout << (ptr1 == arr) << endl; // 1
     return 0;
 }
